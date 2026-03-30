4ccf932e0ffa801862adb6635c3f79efe5f44401
Sumakarkatla89 <sumakarkatla89.com>
2026-03-30 22:01:04 +0530

    iam pushing my code

--- install-packages.sh
+++ install-packages.sh
@@ -1,38 +1,41 @@
 #!/bin/bash
 
 ID=$(id -u)
+
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
- LOGFILE="/tmp/"
+LOGFILE="/tmp/script.log"
 
- VALIDATE(){
+VALIDATE() {
   if [ $1 -ne 0 ]
   then 
-    echo -e "$2 ... $R FAILED $N"
-    exit 1 #you can give other than 0
+    echo -e "$2 ... ${R}FAILED${N}"
+    exit 1
   else
-    echo -e "$2 ... $G SUCCESS $N"
-  fi #fi means reverse of if,indicating condition end
- }
+    echo -e "$2 ... ${G}SUCCESS${N}"
+  fi
+}
+
 if [ $ID -ne 0 ]
 then 
-  echo -e "$R ERROR:: PLEASE RUN THIS SCRIPT THROUGH ROOT USER $N"
+  echo -e "${R}ERROR:: PLEASE RUN THIS SCRIPT THROUGH ROOT USER${N}"
   exit 1
 else
-  echo "you are root user"
-fi # fi means reverse of if,indicating condition end
+  echo -e "${G}you are root user${N}"
+fi
 
-  #echo "all arguments passed: $@"
-for package in $@ #package=git for first time
+for package in $@
 do
-  yum list installed $package #check installed or not
-  if [ $? -ne 0 ] #if not installed
+  yum list installed $package &>> $LOGFILE
+
+  if [ $? -ne 0 ]
   then 
-    yum install $package -y # install the package
-    VALIDATE $? "Installation of $package" #validate
+    yum install $package -y &>> $LOGFILE
+    VALIDATE $? "Installation of $package"
   else
-    echo -e "$Y $package is already installed $N"
+    echo -e "${Y}$package is already installed${N}"
+  fi
 done
