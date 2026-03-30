5b7616272810e86ab9c6def5666c1a7c6488c352
Sumakarkatla89 <sumakarkatla89.com>
2026-03-30 21:29:05 +0530

    iam pushing my code

--- install-packages.sh
+++ install-packages.sh
@@ -6,7 +6,7 @@ G="\e[32m"
 Y="\e[33m"
 if [ $ID -ne 0 ]
 then 
-  echo "$R ERROR:: PLEASE RUN THIS SCRIPT THROUGH ROOT USER $N"
+  echo -e "$R ERROR:: PLEASE RUN THIS SCRIPT THROUGH ROOT USER $N"
   exit 1
 else
   echo "you are root user"
