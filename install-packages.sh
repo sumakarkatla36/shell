2d5e9f08d5aebd1d3034c625142860ed32342735
Sumakarkatla89 <sumakarkatla89.com>
2026-03-30 21:23:50 +0530

    iam pushing my code

--- install-packages.sh
+++ install-packages.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+
+ID=$(id -u)
+if [ $ID -ne 0 ]
+then 
+  echo "ERROR:: PLEASE RUN THIS SCRIPT THROUGH ROOT USER"
+  exit 1
+else
+  echo "you are root user"
+fi # fi means reverse of if,indicating condition end
+
+  echo "all arguments passed: $@"
