b40607bf89a025901ca5903b16d187943e189690
Sumakarkatla89 <sumakarkatla89.com>
2026-04-21 15:39:27 +0530

    iam pushing my code

--- install.sh
+++ install.sh
@@ -7,6 +7,17 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
+LOGFILE="/tmp/$0-"
+
+VALIDATE(){
+  if [$1 -ne 0]
+  then 
+    echo -e "$2 ... $R FAILED $N"
+  else
+     echo -e "$2 ... $G SUCCESS $N"
+  fi
+} 
+
 if [ $ID -ne 0 ]
 then 
   echo -e "${R}ERROR:: PLEASE RUN THIS SCRIPT THROUGH ROOT USER${N}"
@@ -15,4 +26,16 @@ else
   echo -e "${G}you are root user${N}"
 fi
 
-echo "All arguments passed: $@"
+#echo "All arguments passed: $@"
+
+for package in $@
+do
+  yum list installed $package #check installed or not
+  if [$? -ne 0] #if not installed
+  then
+    yum install $package -y #install the package
+    VALIDATE $? "Installation of $package" #validate
+  else
+     echo -e "$package is already installed ... $Y SKIPPING $N"
+  fi
+done
